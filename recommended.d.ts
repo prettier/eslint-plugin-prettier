42abb5d36861690f4bce6ba2769f77ee435cd79d
Daniele <irsooti+code@gmail.com>
2024-08-01 17:03:58 +0200

    feat: prefer `Config` over `FlatConfig` when they're equal

--- recommended.d.ts
+++ recommended.d.ts
@@ -1,5 +1,12 @@
 import { Linter } from 'eslint';
 
-declare const recommendedConfig: Linter.FlatConfig;
+// prettier-ignore
+type IfEqual<A, B, X = A, Y = B> =
+  (<G>() => G extends A & G | G ? 1 : 2) extends
+  (<G>() => G extends B & G | G ? 1 : 2)
+    ? X
+    : Y;
+
+declare const recommendedConfig: IfEqual<Linter.Config, Linter.FlatConfig>;
 
 export = recommendedConfig;
