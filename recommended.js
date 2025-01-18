d789dcd47d71b6c51c2276103c09481f16835e18
Ben Scott <ben@reload.me.uk>
2025-01-18 15:39:03 -0800

    chore: add plugin name to flat recommended config

--- recommended.js
+++ recommended.js
@@ -1,9 +1,10 @@
 const eslintConfigPrettier = require('eslint-config-prettier');
 const eslintPluginPrettier = require('./eslint-plugin-prettier');
 
-// Merge the contents of eslint-config-prettier into every
+// Merge the contents of eslint-config-prettier into config
 module.exports = {
   ...eslintConfigPrettier,
+  name: 'eslint-plugin-prettier/recommended',
   plugins: {
     ...eslintConfigPrettier.plugins,
     prettier: eslintPluginPrettier,
